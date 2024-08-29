cee0063b15df60091a0caa4d3caff3dfe37f1c67
V-Karch <kluna0905@gmail.com>
2024-08-28 20:36:41 -0400

    Logger: added defaults dictionary and prototype rgb_to_escape_sequence function

--- Logger.py
+++ Logger.py
@@ -1,4 +1,14 @@
 class Logger:
+
+    defaults: dict[str, str] = {
+        "default_notice_color": "\033[38;2;r;g;bm",
+        "default_warning_color": "\033[38;2;r;g;bm",
+        "default_error_color": "\033[38;2;r;g;bm",
+        "reset_color": "\033[0m"
+    } # Dictionary of default values for logging
+
+    maximum_hex: int = 16777215
+
     @staticmethod
     def hex_to_rgb(hex: int) -> tuple[int, int, int]:
         """
@@ -7,11 +17,19 @@ class Logger:
 
         For example: 0xFF00FF -> (255, 0, 255)
         """
+
+        if hex > Logger.maximum_hex:
+            raise ValueError("Hex value must be less than or equal to 0xFFFFFF")
+
         red: int = (hex >> 16) & 0xFF
         green: int = (hex >> 8) & 0xFF
         blue: int = hex & 0xFF
 
         return (red, green, blue)
 
-    def __init__(self, NOTICE_COLOR=None, WARNING_COLOR=None, ERROR_COLOR=None) -> None:
+    @staticmethod
+    def rgb_to_escape_sequence(RGB: tuple[int, int, int]) -> str:
+        return f"\033[38;2;{RGB[0]};{RGB[1]};{RGB[2]}m"
+
+    def __init__(self, notice_color=None, warning_color=None, error_color=None) -> None:
         ...
